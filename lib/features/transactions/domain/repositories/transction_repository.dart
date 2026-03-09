e07a610e35f98feb137d6d2c03b01891ed1b6735
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-03-09 23:37:09 +0700

    refactor(transaction): move trans-repo from auth to trans- (typo)

---
+++ lib/features/transactions/domain/repositories/transction_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:dompetku/features/transactions/domain/entities/transaction_entity.dart';
+import '../../../../core/errors/failures.dart';
+import 'package:dompetku/features/transactions/domain/entities/category_entity.dart';
+
+abstract class TransactionRepository {
+  // Transactions
+  Future<Either<Failure, void>> addTransaction(TransactionEntity transaction);
+  Future<Either<Failure, List<TransactionEntity>>> getTransactions();
+  Future<Either<Failure, void>> updateTransaction(
+      TransactionEntity transaction);
+  Future<Either<Failure, void>> deleteTransaction(String id);
+
+  // Sync
+  Future<Either<Failure, void>> syncUnsyncedTransactions();
+
+  // Categories
+  Future<Either<Failure, List<CategoryEntity>>> getCategories();
+
+  // AI OCR
+  Future<Either<Failure, Map<String, dynamic>>> scanReceipt(String imagePath);
+}
