90c9db8d5feca3b19a9c043325cea1d1854c3321
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-03-09 13:03:32 +0700

    feat(auth): adds repositories and sign up usecase

--- lib/features/auth/domain/repositories/auth_repositories.dart
+++ lib/features/auth/domain/repositories/auth_repositories.dart
@@ -1,9 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:dompetku/core/errors/failures.dart';
+
 import '../entities/auth_session_entity.dart';
 
 abstract class AuthRepository {
-  Future<AuthSessionEntity> signUp(
-      String fullName, String email, String password);
-  Future<AuthSessionEntity> signIn(String email, String password);
+  Future<Either<Failure, AuthSessionEntity>> signUp({
+    required String fullName,
+    required String email,
+    required String password,
+  });
+  Future<AuthSessionEntity> signIn({required String email, required password});
   Future<AuthSessionEntity?> checkAuthStatus();
   Future<bool> authenticateBiometric();
   Future<void> logOut();
--- lib/features/auth/domain/usecases/sign_up.dart
+++ lib/features/auth/domain/usecases/sign_up.dart
@@ -1,23 +1,42 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:dompetku/core/errors/failures.dart';
 import 'package:dompetku/features/auth/domain/repositories/auth_repositories.dart';
 import 'package:dompetku/features/auth/domain/entities/auth_session_entity.dart';
+
+class SignUpParams extends Equatable {
+  final String fullName;
+  final String email;
+  final String password;
+
+  const SignUpParams({
+    required this.fullName,
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [fullName, email, password];
+}
 
 class SignUpUseCase {
   final AuthRepository repository;
 
   SignUpUseCase(this.repository);
 
-  Future<Either<Failure, AuthSessionEntity>> call(
-      String fullName, String email, String password) async {
-    if (password.length < 8) {
-      return Left(ValidationFailure('Password minimal 8 karakter'));
+  Future<Either<Failure, AuthSessionEntity>> call(SignUpParams params) async {
+    if (params.password.length < 8) {
+      return const Left(ValidationFailure('Password minimal 8 karakter'));
     }
 
-    if (fullName.trim().isEmpty || email.trim().isEmpty) {
-      return Left(ValidationFailure('Nama dan Email wajib diisi'));
+    if (params.fullName.trim().isEmpty || params.email.trim().isEmpty) {
+      return const Left(ValidationFailure('Nama dan Email wajib diisi'));
     }
 
-    return await repository.signUp(fullName, email, password);
+    return await repository.signUp(
+      fullName: params.fullName,
+      email: params.email,
+      password: params.password,
+    );
   }
 }
